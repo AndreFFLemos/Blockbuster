c14798291f1a554a30254e7b97a697044c102d28
andrefflemos <afontelonga@gmail.com>
2023-07-09 18:40:50 +0100

    refactored the rental services classes

--- src/main/java/Blockbuster/Service/RentalService.java
+++ src/main/java/Blockbuster/Service/RentalService.java
@@ -1,37 +1,72 @@
 package Blockbuster.Service;
 
+import Blockbuster.DTO.RentalDto;
+import Blockbuster.Model.Customer;
 import Blockbuster.Model.Rental;
 import Blockbuster.Repository.RentalRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class RentalService implements RentalServiceInterface {
 
     private RentalRepository rr;
+    private ModelMapper modelMapper;
 
-    @Override
-    public Rental createRental(Rental rental) {
-        return rr.save(rental);
+    @Autowired
+    public RentalService(RentalRepository rr, ModelMapper modelMapper) {
+        this.rr = rr;
+        this.modelMapper = modelMapper;
     }
 
     @Override
-    public void deleteRentalById(int id) {
-        Rental existingRental=rr.findById(id).orElseThrow(()-> new NoSuchElementException("The rental doesn't exist"));
-            rr.deleteById(existingRental.getId());
+    public Optional<RentalDto> createRental(RentalDto rentalDto) {
+        Rental rental= modelMapper.map(rentalDto, Rental.class);
+        rental=rr.save(rental);
+
+        return Optional.of(modelMapper.map(rental,RentalDto.class));
     }
 
     @Override
-    public Rental findRentalById(int id) {
-        return rr.findById(id).orElseThrow(()->new NoSuchElementException("Rental doesn't exist"));
+    public void deleteRental(RentalDto rentalDto) {
+        Rental rental= modelMapper.map(rentalDto, Rental.class);
+        rr.delete(rental);
     }
 
     @Override
-    public List<Rental> findAll() {
-        return rr.findAll();
+    public Optional<RentalDto> findRentalById(int id) {
+        Optional <Rental> rental= rr.findById(id);
+        if (rental.isEmpty()){
+            return Optional.empty();
+        }
+        return Optional.of(modelMapper.map(rental,RentalDto.class));
+
+    }
+
+    @Override
+    public List<RentalDto> findAll() {
+
+        List <Rental> rentals= rr.findAll();
+        if (rentals.isEmpty()){
+            return Collections.emptyList();
+        }
+        return rentals.stream()
+                .map(rental -> modelMapper.map(rental,RentalDto.class))
+                .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public List<Rental> findRentalByCustomer(Customer customer) {
+        return null;
     }
 }
--- src/main/java/Blockbuster/Service/RentalServiceInterface.java
+++ src/main/java/Blockbuster/Service/RentalServiceInterface.java
@@ -1,14 +1,17 @@
 package Blockbuster.Service;
 
+import Blockbuster.DTO.RentalDto;
 import Blockbuster.Model.Customer;
 import Blockbuster.Model.Rental;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RentalServiceInterface {
 
-    public Rental createRental(Rental rental);
-    public void deleteRentalById(int id);
-    public Rental findRentalById(int id);
-    List<Rental> findAll();
+    Optional<RentalDto> createRental(RentalDto rentalDto);
+    void deleteRental(RentalDto rentalDto);
+    Optional<RentalDto> findRentalById(int id);
+    List<RentalDto> findAll();
+    List<Rental> findRentalByCustomer(Customer customer);
 }
