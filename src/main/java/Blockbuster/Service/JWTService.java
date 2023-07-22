83fbb0d5e54a2b3658240be484737e6ba5354329
andrefflemos <afontelonga@gmail.com>
2023-07-22 11:04:34 +0100

    created a JWT class to create Tokens for authentication and authorization

---
+++ src/main/java/Blockbuster/Service/JWTService.java
@@ -0,0 +1,68 @@
+package Blockbuster.Service;
+
+import Blockbuster.Model.Customer;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.jruby.javasupport.ext.JavaTime;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.sql.Time;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.Optional;
+
+@Component
+public class JWTService {
+
+    // Read the private key from a secure configuration or environment variable
+    private static final String privateKey = "supersecretkey"; //this is to exemplify, not real.
+
+    // The expiration time of the token in seconds (1 hour)
+    private static final long tokenDuration = Duration.ofHours(5).getSeconds();
+
+    public String generateToken(Authentication authentication) {
+        // the getPrincipal method returns the Object of the authentication
+        // that is then converted to the POJO.
+        // In this app the POJO is the Customer
+        Customer customer = (Customer) authentication.getPrincipal();
+
+        Instant now = Instant.now();
+        Instant expirationTime = now.plusSeconds(tokenDuration);
+
+        return Jwts.builder()
+                .setSubject(customer.getEmail()) //specifies what should be used to identify the customer
+                .setIssuedAt(Date.from(now)) //when the token was issued
+                .setExpiration(Date.from(expirationTime))//will end at a specified time
+                .signWith(SignatureAlgorithm.HS512, privateKey)// the algorithm used to create the token and the key used that only the server knows
+                .compact(); //compact the token and returns it as a String representation
+    }
+
+    public Optional<String> getUserId(String token) {
+        try {
+            //the Claims class represents the payload of the JSON web token. It provides the details of the token, like the subject,user role, expiration date, etc
+            //by deserializing the payload, the JWT library creats a Claims object with all the claims present in the token.
+            Claims claims = parse(token); //parsing divides the JWT string in Header, Payload and Signature
+            String subject = claims.getSubject();//get the email claim from the token
+            if (subject != null) {
+                return Optional.of((subject));
+            }
+        } catch (Exception e) {
+
+        }
+        return Optional.empty();
+    }
+
+    private Claims parse(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(privateKey)
+                .build()
+                .parseClaimsJws(token) //this method parses the token and if the signature matches it returns a Claims object with the claims and the signature.
+                .getBody(); //and then it returns a Claims object with just the JSON payload(claims).
+    }
+}
